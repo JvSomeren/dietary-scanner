996a480075f5e9b78a0b3aeddb9b8c66a0595658
Joost van Someren <joost.v.someren@gmail.com>
2018-11-06 22:37:28 +0100

    Add `AsyncStorage` service

---
+++ src/services/AsyncStorage/index.js
@@ -0,0 +1,46 @@
+import { AsyncStorage } from 'react-native'
+
+export const _setItem = async ( key: String, value ) => {
+  try {
+    await AsyncStorage.setItem( key, value );
+  } catch ( error ) {
+    // Error saving data
+  }
+};
+
+export const _getItem = async ( key: String ) => {
+  try {
+    const value = await AsyncStorage.getItem( key );
+    if ( value !== null ) {
+      // We have data!!
+      console.log( value );
+    }
+
+    return value;
+  } catch ( error ) {
+    // Error retrieving data
+    console.error( error );
+  }
+};
+
+export const _removeItem = async ( key: String ) => {
+  try {
+    await AsyncStorage.removeItem( key );
+  } catch ( error ) {
+    // Error saving data
+    console.error( error );
+  }
+};
+
+export const _multiGet = async ( keys: Array ) => {
+  try {
+    const arr = await AsyncStorage.multiGet( keys );
+
+    return arr.reduce( ( acc, cur ) => {
+      acc[ cur[ 0 ] ] = cur[ 1 ];
+      return acc;
+    }, {} );
+  } catch ( error ) {
+    console.error( error );
+  }
+};
