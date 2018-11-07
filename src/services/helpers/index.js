af06a5d5da49c894cde1423433730bb55e04b846
Joost van Someren <joost.v.someren@gmail.com>
2018-11-07 21:19:21 +0100

    Add `listToArray` helper functions

--- src/services/helpers/index.js
+++ src/services/helpers/index.js
@@ -1,5 +1,7 @@
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+export const capitalize = String.prototype.capitalize = function () {
+  return this.charAt( 0 ).toUpperCase() + this.slice( 1 );
 };
 
-export const capitalize = String.prototype.capitalize();
+export const toListArray = function ( obj: Object ) {
+  return Object.keys(obj).map((val) => { return { key: val } } );
+};
