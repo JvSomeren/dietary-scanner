9d2ef71ad60e2eafeafcb2dc15172ec00e3c72bd
Joost van Someren <joost.v.someren@gmail.com>
2018-11-09 17:15:19 +0100

    Move `LanguageList` to component on `Welcome`-screen

--- src/scenes/Welcome/index.js
+++ src/scenes/Welcome/index.js
@@ -8,28 +8,14 @@ import { updateLanguage } from "../../services/settings/service";
 
 import base from '../../styles/base.scss'
 import styles from './styles.scss'
+import LanguageList from "../../components/LanguageList";
 
 type Props = {};
 
 class Welcome extends Component<Props> {
   constructor( props ) {
     super( props );
-
-    this.languages = i18n.translations;
   }
-
-  _onPressItem = ( language ) => {
-    if ( language !== this.props.language )
-      this.props.updateLanguage( language );
-  };
-
-  _subtitle = ( item ) => {
-    return item === this.props.language ? i18n.t( 'selected' ).capitalize() : null;
-  };
-
-  _leftIcon = ( item ) => {
-    return item === this.props.language ? { name: 'check' } : { name: 'remove' };
-  };
 
   render() {
     const { navigate } = this.props.navigation;
@@ -43,20 +29,7 @@ class Welcome extends Component<Props> {
             pressing <Text>{i18n.t( 'next' )}</Text>.</Text>
         </View>
 
-        <List containerStyle={[ base.flex, base.stretch ]}>
-          {
-            this.props.languages.map( ( item ) => (
-              <ListItem
-                key={item}
-                title={i18n.t( 'Translations.' + item ).capitalize()}
-                subtitle={this._subtitle( item )}
-                hideChevron
-                leftIcon={this._leftIcon( item )}
-                onPress={() => this._onPressItem( item )}
-              />
-            ) )
-          }
-        </List>
+        <LanguageList />
 
         <View style={base.flexCenter}>
           <Button
