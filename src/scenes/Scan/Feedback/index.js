93a42f931ae584d5b46a4c502f5966a48736c8e7
Joost van Someren <joost.v.someren@gmail.com>
2018-11-10 19:49:36 +0100

    Fix incorrect import path

--- src/scenes/Scan/Feedback/index.js
+++ src/scenes/Scan/Feedback/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from "react-redux";
 
-import base from '../../styles/base.scss'
+import base from '../../../styles/base.scss'
 
 type Props = {};
 
