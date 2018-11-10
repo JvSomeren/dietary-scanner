1ec09def6f27e840c649b40be7bb0f8246ad242d
Joost van Someren <joost.v.someren@gmail.com>
2018-11-10 20:56:27 +0100

    Fix Android get system locale

--- src/i18n.js
+++ src/i18n.js
@@ -4,7 +4,7 @@ import i18n from 'i18n-js'
 import en from './translations/en.json'
 import nl from './translations/nl.json'
 
-i18n.locale = RNLanguages.language;
+i18n.locale = RNLanguages.language.match(/[a-z]{2}/)[0];
 i18n.fallbacks = true;
 i18n.translations = { en, nl };
 
